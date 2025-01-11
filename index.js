399bfbe8971f2c05986ba360ad5f7221cd10a254
Pritesh Mishra <priteshmishra2125@gmail.com>
2025-01-11 06:19:01 +0530

    resolve deploy issue

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
@@ -8,17 +7,10 @@ dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-//middlewares
 app.use(express.json());
-// app.use(cookieParser());
 
-// const corsOptions = {
-//     origin: process.env.URL,
-//     credentials: true
-// }
-// app.use(cors(corsOptions));
 
- //api
+ //my api
 app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
